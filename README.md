89daf766a0d7b92374306153d97da12430a219cb
hihuss <hihusssss@gmail.com>
2024-09-22 14:45:26 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/2i7xrrflooscklhr?svg=true)](https://ci.appveyor.com/project/hihussss/htmlform)
+
+
+https://hihussss.github.io/htmlForm/
--- src/test/popover.test.js
+++ src/test/popover.test.js
@@ -1,13 +1,15 @@
 import puppeteer from "puppeteer";
 
 async function getPopover() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('http://192.168.1.33:8080');
+    await page.goto('https://hihussss.github.io/htmlForm/');
     await page.waitForSelector('.button');
     await page.click('.button');
     await page.waitForSelector('.tooltip');
     await page.waitForSelector('.tooltip_active');
+    const html = await page.$eval('.tooltip_text', e => e.innerHTML);
+    expect(html).toBe('Ты сделал правильный выбор!');
     await browser.close();
 }
 
